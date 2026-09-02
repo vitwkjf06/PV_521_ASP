a1e8af5769734fc481c601b307cbc22bf1037bf2
vitwkf06 <vboec06@gmail.com>
2026-09-02 21:40:04 +0300

    Tasklist in progress

---
+++ Blazor/TODOlist/Task.cs
@@ -0,0 +1,16 @@
+﻿namespace TODOlist
+{
+    public class Task
+    {
+        public string Description { get; set; }
+        public bool DONE { get; set; }
+        public override bool Equals(object? obj)
+        {
+            return this.Description.Equals
+                (
+                (other as  TODOlist.Task).Description,
+                StringComparison.OrdinalIgnoreCase
+                );
+        }
+    }
+}
