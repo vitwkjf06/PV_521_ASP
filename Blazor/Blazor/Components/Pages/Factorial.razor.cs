ec94e777f8a72596ba057ab4ac102dd19005a73a
vitwkf06 <vboec06@gmail.com>
2026-09-02 17:13:08 +0300

    Factorial DONE

---
+++ Blazor/Blazor/Components/Pages/Factorial.razor.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Factorial
+    {
+        int limit = 0;
+        BigInteger factorial = 1;
+        void Calculate()
+        {
+            factorial = 1;
+            for (int i = 1; i <= limit; i++)
+                factorial *= i;
+        }
+    }
+}
